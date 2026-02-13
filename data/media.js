80abc529199743f44f25d1ff9a130017a6afdeda
hachi <120341724+Hachinijuu@users.noreply.github.com>
2026-02-13 08:52:49 -0500

    Let's try this again
    Changed routes, slowly adding files for video tab functioning

---
+++ data/media.js
@@ -0,0 +1,90 @@
+export const media = [
+/*
+{
+    id: "PROJECT_ITEM_ALT",
+    type: "visual",
+    description: "ITEM"
+    src: "/assets/media/PROJECTFOLDER/PROJECT_ITEM_ALT",
+    date: "2026MMDD",
+    platform: "plat",
+    commentary: "comment"
+},
+*/
+//--------------------------------------------
+
+// SPRITES
+
+{
+    id: "SPRITES_cover.png",
+    type: "visual",
+    description: "Cover Art",
+    src: "/assets/media/_sprites/SPRITES_cover.png",
+    date: "202510",
+    platform: "All DSPs",
+    commentary: "...The entire animation is technically still there..."
+},
+
+{
+    id: "SPRITES_cover_anim.gif",
+    type: "visual",
+    description: "Cover Art",
+    src: "/assets/media/_sprites/SPRITES_cover_anim.gif",
+    date: "202602",
+    platform: "vibware.com",
+    commentary: "Alive."
+},
+
+{
+    id: "SPRITES_logorotation_small.gif",
+    type: "visual",
+    description: "Asset",
+    src: "/assets/media/_sprites/SPRITES_logorotation_small.gif",
+    date: "202602",
+    platform: "vibware.com, tiktok",
+    commentary: "One of the building blocks."
+},
+
+{
+    id: "SPRITES_visualizer.gif",
+    type: "visual",
+    description: "Visualizer",
+    src: "/assets/media/_sprites/SPRITES_visualizer.gif",
+    date: "202602",
+    platform: "tiktok",
+    commentary: "Welcome."
+},
+
+{
+    id: "SPRITES_visualizer_alt.gif",
+    type: "visual",
+    description: "Visualizer",
+    src: "/assets/media/_sprites/SPRITES_visualizer_alt.gif",
+    date: "202602",
+    platform: "Spotify",
+    commentary: "Watch them greet you. meet the sprites."
+},
+
+
+// SUMMERING
+
+{
+    id: "summering_cover.png",
+    type: "visual",
+    description: "Cover Art",
+    src: "/assets/media/_summering/summering_cover.png",
+    date: "202602",
+    platform: "All DSPs",
+    commentary: "...wasn't hoping for us to end this way..."
+},
+
+{
+    id: "summering_cover_anim.gif",
+    type: "visual",
+    description: "Cover Art",
+    src: "/assets/media/_summering/summering_cover_anim.gif",
+    date: "202602",
+    platform: "vibware.com",
+    commentary: "Take it easy, turn the page. It's just another summer day."
+}
+
+]
